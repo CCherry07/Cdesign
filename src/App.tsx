6e4f91dd495986668d13bb0107fdb0e5a62e616a
CCherry07 <2405693142@qq.com>
2022-06-20 23:44:39 +0800

    feat:  test CButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import './App.css'
+
+import CButton ,{  ButtonType , ButtonSize }from './c_button';
 
 function App() {
   return (
     <div className="App">
-      
+      <CButton 
+        btnType={ ButtonType.Link }
+        size={ButtonSize.Large} 
+        href='http://baidu.com'>
+        hello 
+      </CButton>
+      <CButton 
+        btnType={ ButtonType.Default } 
+        size={ButtonSize.Small}
+        disabled={true}> 
+        撒大大 
+      </CButton>
+      <CButton 
+        btnType={ ButtonType.Primary } 
+        size={ButtonSize.Large}> 
+        cherry 
+      </CButton>
     </div>
   )
+
 }
 
 export default App
