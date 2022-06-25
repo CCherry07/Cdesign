8dfb3a3c84357e81bb3791187b955dd87596e1bb
CCherry07 <2405693142@qq.com>
2022-06-25 11:27:03 +0800

    feat:  create mouse event contrl item show

--- src/components/menu/SubMenu.tsx
+++ src/components/menu/SubMenu.tsx
@@ -16,11 +16,33 @@ const SubMenu:React.FC<SubMenuProps> = (props)=>{
   const classes = classNames("menu-item submenu-item" , className , {
     "is-active":context.index === index
   })
+
   const handleClick = (e:React.MouseEvent)=>{
     e.stopPropagation()
     setOpen(!menuOpen)
   }
+
+  let timer:any
+  const handleHover = (e:React.MouseEvent , toggle:boolean)=>{
+    clearTimeout(timer)
+    e.stopPropagation()
+    timer =setTimeout(()=>{
+      setOpen(toggle)
+    },300)
+  }
+
+  const clickEvents = context.mode ==="vertical" ? {
+    onClick:handleClick
+  }:null
+  const hoverEvents = context.mode !=="vertical" ? {
+    onMouseEnter:(e:React.MouseEvent)=>handleHover(e,true),
+    onMouseLeave:(e:React.MouseEvent)=>handleHover(e,false)
+  }:null
+  
   const renderChildren = () =>{
+    const subMenuClasses = classNames("viking-submenu", {
+      "menu-opened":menuOpen
+    })
     const childrenEls = React.Children.map(children,(child , index)=>{
       const childEl = child as React.FunctionComponentElement<MenuItemProps>
       const { name } = childEl.type      
@@ -31,14 +53,14 @@ const SubMenu:React.FC<SubMenuProps> = (props)=>{
       }
     })
     return (
-      <ul className="viking-submenu">
+      <ul className={subMenuClasses}>
         {childrenEls}
       </ul>
     )
   }
   return (
-    <li key={index} className={classes}>
-      <div className="submenu-title">
+    <li key={index} className={classes} {...hoverEvents}>
+      <div className="submenu-title" {...clickEvents}>
         {title}
       </div>
       {renderChildren()}
