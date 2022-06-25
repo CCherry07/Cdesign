4d695738041229866d1add275e3cd8e3399dea22
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-06-25 22:46:58 +0800

    feat: better onChange

--- src/components/switch/switch.tsx
+++ src/components/switch/switch.tsx
@@ -1,19 +1,22 @@
-import React, { FC, useState, cloneElement } from 'react';
+import React, { FC, useState } from 'react';
 
 export type SwitchSize = 'lg' | 'sm';
 
 export interface SwitchProps {
   defaultChecked?: boolean,
   labelName?: string,
-  onChange?: () => any
+  onChange?: (value: boolean) => any
 }
 
-export const Switch: FC<SwitchProps> = ({ defaultChecked = false, labelName, onChange }) => {
+export const Switch: FC<SwitchProps> = ({ defaultChecked, labelName, onChange }) => {
   const [state, setState] = useState(defaultChecked);
   return (
-    <div className='switch' onClick={() => setState(!state)} >
+    <div className='switch' onClick={() => {
+      setState(!state);
+      onChange!(state as boolean);
+    }} >
       <label htmlFor={labelName}></label>
-      <input type="checkbox" name={labelName} checked={state} onChange={onChange} />
+      <input type="checkbox" name={labelName} checked={state} />
       <span className="slider" />
     </div>
   );
