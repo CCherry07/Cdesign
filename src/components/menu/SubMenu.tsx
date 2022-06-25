08f9bb1f800415fd567c8b8ee8afb6e4c9758453
CCherry07 <2405693142@qq.com>
2022-06-25 10:45:47 +0800

    feat:  judgment submenu children are menuitem

--- src/components/menu/SubMenu.tsx
+++ src/components/menu/SubMenu.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames"
 import React, { useContext } from "react"
 import { MenuContext }  from './MenuContext'
+import { MenuItemProps } from "./menuItem"
 export interface SubMenuProps {
   index?:number
   title:string
@@ -14,11 +15,28 @@ const SubMenu:React.FC<SubMenuProps> = (props)=>{
   const classes = classNames("menu-item submenu-item" , className , {
     "is-active":context.index === index
   })
+  const renderChildren = () =>{
+    const childrenEls = React.Children.map(children,(child , index)=>{
+      const childEl = child as React.FunctionComponentElement<MenuItemProps>
+      const { name } = childEl.type      
+      if (name === "MenuItem") {
+        return React.cloneElement(childEl , { index })
+      }else{
+        console.error("warning: Menu has is a child is not a MenuItem");
+      }
+    })
+    return (
+      <ul className="viking-submenu">
+        {childrenEls}
+      </ul>
+    )
+  }
   return (
     <li key={index} className={classes}>
       <div className="submenu-title">
         {title}
       </div>
+      {renderChildren()}
     </li>
   )
 }
