18217ae0ba457c4bd8cbc07c00aa7240a15bae1b
CCherry07 <2405693142@qq.com>
2022-06-26 14:25:14 +0800

    feat:  Transition component

--- src/components/transition/transition.tsx
+++ src/components/transition/transition.tsx
@@ -1,6 +1,27 @@
+import { CSSTransition  } from 'react-transition-group'
+import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
 
-const Transition = ()=>{
-  
+type QueueTransitionName = "zoom-in-top"|"zoom-in-left"|"zoom-in-bottom"|"zoom-in-right"
+
+type TransitionProps = {
+  queueAnim?:QueueTransitionName
+  children?:React.ReactNode
+} & CSSTransitionProps
+
+const Transition:React.FC<TransitionProps> = (props)=>{
+  const { children , classNames ,queueAnim , ...restProps} = props
+  return (
+    <CSSTransition 
+      classNames={classNames || queueAnim}
+      {...restProps}>
+        { children }
+    </CSSTransition>
+  )
+}
+
+Transition.defaultProps = { 
+  unmountOnExit:true,
+  appear:true
 }
 
 export default Transition
