00252d46fcc79bae70227ca959a2aa097b2edef1
CCherry07 <2405693142@qq.com>
2022-06-28 23:04:47 +0800

    workflow：create autocomplete component

---
+++ src/components/autoComplete/autoComplete.tsx
@@ -0,0 +1,40 @@
+import { ChangeEvent, useState } from 'react'
+import Input,{ InputProps } from '../input/input'
+
+export interface DataSourceItemObject {
+  value: string;
+  text: string;
+}
+export interface AutoCompleteProps extends Omit<InputProps , "onSelect"> {
+  dataSource?:DataSourceItemObject[],
+  filterOption?:(inputValue: string,options: DataSourceItemObject[])=>DataSourceItemObject[]
+  onSelect?:(options:DataSourceItemObject)=>void
+}
+
+export const AutoComplete:React.FC<AutoCompleteProps> = (props)=>{
+  const { filterOption , onSelect ,value , dataSource, ...restprops  } = props
+  const [inputValue,setInputValue] = useState(value)
+  const [options , setOptions] = useState(props.dataSource||[])
+  const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
+    const value = e.target.value.trim()
+    setInputValue(value)
+    if (value) {
+      const filterOptions = filterOption?.(value,dataSource||[])||[]
+      console.log(filterOptions);
+      setOptions(filterOptions)
+    }else{
+      setOptions([])
+    }
+  }
+  return (
+    <div className='viking-auto-complete'>
+      <Input 
+      value={inputValue}
+      onChange={handleChange} 
+      { ...restprops }/>
+    </div>
+  )
+}
+
+
+export default AutoComplete
