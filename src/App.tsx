c50ba9180ebcd8be282610be78ace912bbe389b9
CCherry07 <2405693142@qq.com>
2022-07-09 21:58:21 +0800

    feat:  add progress component style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Upload } from './components/upload/Upload';
+import { Upload, UploadFile } from './components/upload/Upload';
 import "./app.scss"
 function App() {
- const  handleProgress=(o,file)=>{
+ const  handleProgress=(o:number,file:File)=>{
   console.log(o);
   }
+  const defalutFileList:UploadFile[] = [
+    {uid:"1",size:432,name:"file1",status:"error",percent:50},
+    {uid:"2",size:432,name:"file2",status:"success",percent:50},
+    {uid:"3",size:432,name:"file3",status:"uploading",percent:50}
+  ]
   return (
     <div className="App">
-      <Upload action='https://jsonplaceholder.typicode.com/posts' onProgress={handleProgress}></Upload>
+      <Upload action='https://jsonplaceholder.typicode.com/posts' defaultFileList={defalutFileList} onProgress={handleProgress}></Upload>
     </div>
   );
 }
