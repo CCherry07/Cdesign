5abe4afd28aabc532a2532581fb36b61bd9f7a7e
CCherry07 <2405693142@qq.com>
2022-07-09 21:37:49 +0800

    feat:  create progress component

---
+++ src/components/progress/Progress.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import  { ThemeProps } from '../icon/icon'
+export interface ProgressProps {
+  percent:number
+  strokeHight?:number
+  showText?:boolean
+  styles?:React.CSSProperties
+  theme?:ThemeProps
+}
+
+export const Progress:React.FC<ProgressProps> = (props)=> {
+  const {percent , showText , strokeHight ,styles ,theme} = props
+  return (
+    <div className="viking-progress-bar" style={styles}>
+      <div 
+        className="viking-progress-bar-outer"
+        style={{height:`${strokeHight}px`}}
+      >
+        <div className={`viking-progress-bar-inner color-${theme}`} style={{width:`${percent}%`}}>
+          { showText && <span className="inner-text">{ `${percent}%` }</span> }
+        </div>
+      </div>
+    </div>
+  )
+}
+Progress.defaultProps = {
+  strokeHight:15,
+  showText:true,
+  theme:"primary"
+}
+
+export default Progress
