bd535de603f6d9f8adaf82bfca5cafc4f6ebdeff
CCherry07 <2405693142@qq.com>
2022-07-10 17:08:36 +0800

    style:  change code line

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { action } from '@storybook/addon-actions'
 import Button  from './button';
+
 export default {
   title: 'Button',
   component: Button,
