fb764f990bf6c0b8481fc4e3abdfaa45db8850eb
CCherry07 <2405693142@qq.com>
2022-07-11 11:22:10 +0800

    refactor:  refactor hooks file path

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,9 @@
 import { useDebounce } from './useDebounce'
-import { useClickTargetOutsite } from './useClickTargetOutSide'
-import { useDocTitle } from './useDocTitle'
-export { useDebounce, useClickTargetOutsite , useDocTitle };
+import { useClickTargetOutsite } from './dom/useClickTargetOutSide'
+import { useDocTitle } from './dom/useDocTitle'
+import { useMuntedRef } from './dom/useMuntedRef'
+
+export { useDebounce};
+
+// dom hooks
+export { useClickTargetOutsite , useDocTitle ,useMuntedRef }
