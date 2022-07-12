28cded7aa56d3217f8e68863c9e2d341e565b646
CCherry07 <2405693142@qq.com>
2022-07-12 22:21:56 +0800

    feat:  `useMount useUnMount`

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,8 +2,9 @@ import { useDebounce } from './useDebounce'
 import { useClickTargetOutsite } from './dom/useClickTargetOutSide'
 import { useDocTitle } from './dom/useDocTitle'
 import { useMuntedRef } from './dom/useMuntedRef'
-
+import { useMount } from './dom/useMount'
+import { useUnMount } from './dom/useUnMount'
 export { useDebounce};
 
 // dom hooks
-export { useClickTargetOutsite , useDocTitle ,useMuntedRef }
+export { useClickTargetOutsite , useDocTitle ,useMuntedRef , useMount ,useUnMount }
