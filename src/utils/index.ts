ad1ac93c1258871c63654693327b97ced45423df
CCherry07 <2405693142@qq.com>
2022-07-22 23:07:59 +0800

    feat:  `subset`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,13 @@
 export const isVoid =(value:unknown)=>value === (undefined || null || "") ? true :false
+export const subset = <
+  O extends { [key in string]: unknown },
+  K extends keyof O
+>(
+  obj: O,
+  keys: K[]
+) => {
+  const filteredEntries = Object.entries(obj).filter(([key]) =>
+    keys.includes(key as K)
+  );
+  return Object.fromEntries(filteredEntries) as Pick<O, K>;
+};
