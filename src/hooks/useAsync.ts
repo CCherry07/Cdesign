e9744613e5f0c71410a4405c6a3695281a62ca99
CCherry07 <2405693142@qq.com>
2022-08-02 19:39:56 +0800

    feat:  style hooks

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,67 +1,67 @@
 import { useCallback, useState } from "react"
 import { useMuntedRef } from "./dom/useMuntedRef"
 
-interface State<D>{
-  error:Error|null
-  data:D | null
-  status:"idle"|"loading"|"error"|"success"
+interface State<D> {
+  error: Error | null
+  data: D | null
+  status: "idle" | "loading" | "error" | "success"
 }
-const defaultInitState:State<null> ={
-  status:"idle",
-  data:null,
-  error:null
+const defaultInitState: State<null> = {
+  status: "idle",
+  data: null,
+  error: null
 }
 
 const defaultConfig = {
-  processErrorBySelf:false
+  processErrorBySelf: false
 }
 
-export const useAsync = <D>(initState?:State<D>,initConfig?:typeof defaultConfig)=>{
-  const config = {...defaultConfig , ...initConfig }
-  const [ state , setState ] = useState<State<D>>({
+export const useAsync = <D>(initState?: State<D>, initConfig?: typeof defaultConfig) => {
+  const config = { ...defaultConfig, ...initConfig }
+  const [state, setState] = useState<State<D>>({
     ...defaultInitState,
     ...initState
   })
-  const [ retry , setReTry ] = useState(()=>()=>{})
-  const setData = (data:D) => setState({
+  const [retry, setReTry] = useState(() => () => { })
+  const setData = (data: D) => setState({
     data,
-    status:"success",
-    error:null
+    status: "success",
+    error: null
   })
-  const setError = (error:Error) => setState({
-    data:null,
-    status:"error",
+  const setError = (error: Error) => setState({
+    data: null,
+    status: "error",
     error
   })
-  
-  const muntedRef = useMuntedRef() 
 
-  const run  = useCallback(( promise:Promise<D> ,runConfig?:{ retry:()=>Promise<D> } )=>{
+  const muntedRef = useMuntedRef()
+
+  const run = useCallback((promise: Promise<D>, runConfig?: { retry: () => Promise<D> }) => {
     if (!promise || !promise.then) {
       throw new Error("please Pass In The PromiseType !");
-    } 
-    setReTry(()=>()=>{
+    }
+    setReTry(() => () => {
       if (runConfig?.retry) {
-        run(runConfig.retry(),runConfig)
+        run(runConfig.retry(), runConfig)
       }
     })
-    setState({...state , status:"loading"})
-    return promise.then(data=>{
+    setState({ ...state, status: "loading" })
+    return promise.then(data => {
       if (muntedRef.current) {
         setData(data)
       }
       return data
-    },(error=>{
+    }, (error => {
       setError(error)
       if (config.processErrorBySelf) return Promise.reject(error)
       return error
     }))
-  },[muntedRef,retry,setData,setError,state])
+  }, [muntedRef, retry, setData, setError, state])
   return {
-    isIdle:state.status ==="idle",
-    isLoading:state.status ==="loading",
-    isError:state.status ==="error",
-    isSuccess:state.status ==="success",
+    isIdle: state.status === "idle",
+    isLoading: state.status === "loading",
+    isError: state.status === "error",
+    isSuccess: state.status === "success",
     run,
     retry,
     setData,
