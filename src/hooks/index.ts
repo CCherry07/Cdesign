8566df7bbaf1a76c8061881e8f15376b335b9cd2
CCherry07 <2405693142@qq.com>
2022-09-10 08:49:59 +0800

    feat:  `useLocalStorageState`

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,9 +3,11 @@ import { useClickTargetOutsite } from './dom/useClickTargetOutSide'
 import { useDocTitle } from './dom/useDocTitle'
 import { useMuntedRef } from './dom/useMuntedRef'
 import { useMount } from './dom/useMount'
+import { useLocalStorageState } from './dom/useLocalStorageState';
 import { useUnMount } from './dom/useUnMount'
 import { useAsync } from './useAsync'
-export { useDebounce , useAsync};
+
+export { useDebounce, useAsync };
 
 // dom hooks
-export { useClickTargetOutsite , useDocTitle ,useMuntedRef , useMount ,useUnMount }
+export { useClickTargetOutsite, useDocTitle, useMuntedRef, useMount, useUnMount, useLocalStorageState }
