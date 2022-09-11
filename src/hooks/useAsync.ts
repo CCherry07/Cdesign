8a727bde1765913e1a03616e8f4d45a69e7042d5
CCherry07 <2405693142@qq.com>
2022-09-11 15:22:28 +0800

    fix:  unSafeDispatch with `useSafeDispatch` hook

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,5 +1,6 @@
 import { useCallback, useReducer, useState } from "react"
 import { useMuntedRef } from "./dom/useMuntedRef"
+import { useSafeDispatch } from './dom/useSafeDispatch';
 
 interface State<D> {
   error: Error | null
@@ -18,10 +19,11 @@ const defaultConfig = {
 
 export const useAsync = <D>(initState?: State<D>, initConfig?: typeof defaultConfig) => {
   const config = { ...defaultConfig, ...initConfig }
-  const [state, dispatch] = useReducer((state: State<D>, action: Partial<State<D>>) => ({ ...state, ...action }), {
+  const [state, unSafeDispatch] = useReducer((state: State<D>, action: Partial<State<D>>) => ({ ...state, ...action }), {
     ...defaultInitState,
     ...initState
   })
+  const dispatch = useSafeDispatch(unSafeDispatch)
   const [retry, setReTry] = useState(() => () => { })
   const setData = (data: D) => dispatch({
     data,
