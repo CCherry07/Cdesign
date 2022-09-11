734f287ab30e1012aa75d53a0314741e316ec2da
CCherry07 <2405693142@qq.com>
2022-09-11 15:17:57 +0800

    fix:  useLayoutEffect

--- src/hooks/dom/useMuntedRef.tsx
+++ src/hooks/dom/useMuntedRef.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from "react"
+import { useLayoutEffect, useRef } from "react"
 
 // 判断 首屏dom 是否已经挂载完成
 export const useMuntedRef = () => {
   const muntedRef = useRef(false)
-  useEffect(() => {
+  useLayoutEffect(() => {
     muntedRef.current = true
     return () => {
       muntedRef.current = false
